6deb951c0fd42eb6eee8f4c5cf2afd65311f4476
CyberEugene <36011795+CyberEugene@users.noreply.github.com>
2018-01-31 21:47:08 +0200

    Push base classes

---
+++ HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApiService.Models;
+
+namespace WebApiService.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepositiory repositiory { get; set; }
+
+        public HomeController(IRepositiory repo)
+        {
+            repositiory = repo;
+        }
+
+        public ViewResult Index() => View(repositiory.Reservations);
+
+        [HttpPost]
+        public IActionResult AddReservation(Reservation reservation)
+        {
+            repositiory.AddReservation(reservation);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ ReservationController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApiService.Models;
+
+namespace WebApiService.Controllers
+{
+    [Route("api/[controller]")]
+    public class ReservationController : Controller
+    {
+        private IRepositiory repositiory { get; set; }
+
+        public ReservationController(IRepositiory repo)
+        {
+            repositiory = repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Reservation> Get() => repositiory.Reservations;
+
+        [HttpGet("{id}")]
+        public Reservation Get(int id) => repositiory[id];
+
+        [HttpPost]
+        public Reservation Post([FromBody] Reservation res) =>
+            repositiory.AddReservation(new Reservation() {ClientName = res.ClientName, Location = res.Location});
+
+        [HttpPut]
+        public Reservation Put([FromBody] Reservation res) =>
+            repositiory.UpdateReservation(res);
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) => repositiory.DeleteReservation(id);
+
+    }
+}
